8a41a2d2b4465252ee7d213cb67395ae18faf0de
diogeneshas <diogenes.henrique.br@gmail.com>
2022-03-07 08:22:49 -0300

    feat: Usando Scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,11 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
-  def self.search(page, term)
-    Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
-  end
+  scope :_search_, ->(page, term) {
+    includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
+  }
 
-  def self.last_questions(page)
-    Question.includes(:answers).order('created_at desc').page(page).per(5)
-  end
+  scope :last_questions, ->(page) {
+    includes(:answers).order('created_at desc').page(page).per(5)
+  }
 end
