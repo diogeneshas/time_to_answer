46f5f156a5b7ac3c13033575bd132bdbdfa619db
diogeneshas <diogenes.henrique.br@gmail.com>
2022-03-08 10:13:07 -0300

    feat: Filtrando as questoes por assunto

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,17 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
   scope :_search_, ->(page, term) {
-    includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
+    includes(:answers, :subject).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
   }
 
   scope :last_questions, ->(page) {
-    includes(:answers).order('created_at desc').page(page).per(5)
+    includes(:answers, :subject).order('created_at desc').page(page).per(5)
   }
 end
