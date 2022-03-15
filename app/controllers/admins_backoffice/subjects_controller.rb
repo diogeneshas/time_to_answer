657be201880268f070db504f2dfea87f2d7efd39
diogeneshas <diogenes.henrique.br@gmail.com>
2022-03-14 23:53:11 -0300

    feat: Trabalhando com PDF

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -2,7 +2,10 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   before_action :set_subject, only: [:edit, :update, :destroy]
 
   def index
-    @subjects = Subject.all.order(:description).page(params[:page]).per(10)
+    respond_to do |format|
+      format.html { @subjects = Subject.all.order(:description).page(params[:page]).per(10) }
+      format.pdf { @subjects = Subject.all.order(:description) }
+    end
   end
 
   def new
